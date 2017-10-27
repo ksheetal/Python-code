0db313e30c1ff45d879ff6613de3a0b9f801f55f
Sheetal Kumar <31645349+ksheetal@users.noreply.github.com>
2017-10-27 12:12:16 +0530

    Add files via upload

--- merge.py
+++ merge.py
@@ -1,5 +1,5 @@
-#import pdb
-#pdb.set_trace()
+import pdb
+pdb.set_trace()
 
 def merge(list1, list2):
     '''
@@ -20,8 +20,16 @@ def merge(list1, list2):
     if list1 != [] and list2 != []:
        if list1[0] <= list2[0]:
           list3.append(list1[0])
-          list3 = list3 +  merge(list1[1:], list2)
+          return list3 +  merge(list1[1:], list2)
        if list1[0] > list2[0]:
           list3.append(list2[0])
-          list3 = list3 +  merge(list1, list2[1:])
-    return list3
+          return list3 +  merge(list1, list2[1:])
+
+def main():
+    list1=[int(x) for x in input("Enter list1 : ").split()]
+    list2=[int(x) for x in input("Enter list2 : ").split()]
+    merge(list1,list2)
+    print("list after sorting",merge(list1,list2))
+
+if __name__=="__main__":
+    main()
